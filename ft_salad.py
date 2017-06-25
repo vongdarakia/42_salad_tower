7239da8212641591877703366d46c7aca113a1bc
Akia Vongdara <vongdarakia@gmail.com>
2017-06-25 04:31:44 -0700

    SQLite fully implemented. Working on query for graph data

--- ft_salad.py
+++ ft_salad.py
@@ -62,12 +62,24 @@ def listen():
     checkpoint = gmtime().tm_min
     t_minute_values = []
     h_minute_values = []
-    with app.app_context():
-        db = get_db()    
-        cur = db.cursor()
-        if ser:
-            while True:
-                if gmtime().tm_min == checkpoint + 1:
+    if ser:
+        while True:
+            # with app.app_context():
+            #     hum_record = get_db().execute('SELECT * FROM humidity')
+            #     hum = hum_record.fetchall()
+            #     hum_record.close()
+            #     print (hum)
+            if gmtime().tm_min == checkpoint + 1:
+                with app.app_context():
+                    hum_record = get_db().execute('SELECT humidity FROM humidity ORDER BY time DESC')
+                    hum = hum_record.fetchall()
+                    hum_record.close()
+                    print ('Here is what we got')
+                    print (hum)
+                    # print (hum[0])
+                    # print (hum[1])
+                    db = get_db()    
+                    cur = db.cursor()
                     read_serial = ser.readline()
                     print(read_serial)
                     # read_serial = read_serial.strip()
@@ -84,40 +96,23 @@ def listen():
                         continue
                     print (h_minute_values)
                     print (t_minute_values)
-                    cur.execute('INSERT INTO humidity VALUES (?,?,?)', (1,time_taken, h_minute_values[-1]))
-                    cur.execute('INSERT INTO temperature VALUES (?,?,?)', (1,time_taken, t_minute_values[-1]))
+                    time_taken = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                    print ('Last Entry')
+                    print (h_minute_values[-1])
+                    print (t_minute_values[-1])
+                    if h_minute_values[-1] < 100:
+                        cur.execute('INSERT INTO humidity VALUES (?,?)', (time_taken, h_minute_values[-1]))
+                    if t_minute_values[-1] < 100:
+                        cur.execute('INSERT INTO temperature VALUES (?,?)', (time_taken, t_minute_values[-1]))
                     db.commit()
-                    cur.execute('SELECT * FROM humidity, temperature')
-                    print (cur.fetchone())
-
-                    # if len(split) != 2:
-                    #     continue
-
-                    # if 'Humidity' in split[0]:
-                    #     hum_data = split[0].split(' ');
-                    #     if (len(hum_data) != 2):
-                    #         continue
-                    #     hum = hum_data[1]
-                    # else:
-                    #     continue
-
-                    # if 'Temperature' in split[1]:
-                    #     tmp_data = split[1].split(' ');
-                    #     if (len(tmp_data) != 2):
-                    #         continue
-                    #     tmp = tmp_data[1]
-                    # else:
-                    #     continue
-
-                    # time_taken = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-                    # appendDataCSV(data_file, makeData(hum, tmp, time_taken));
+                    # for row in cur.execute('SELECT * FROM temperature'):
+                    #     print ('Temp')
+                    #     print (row)
+                    # for row2 in cur.execute('SELECT * FROM humidity'):
+                    #     print ('Hum')
+                    #     print (row2)
                     checkpoint = checkpoint + 1
                     sleep(2)
-            # else:
-            #     print ('Not time')
-            #     print (gmtime().tm_min)
-            #     print (gmtime().tm_sec)
-    print("listening over")
 
 @app.route("/")
 def index():
@@ -128,39 +123,36 @@ def about():
     return render_template('about.html')
 
 @app.route("/data")
-def dataPi(sensorData=None):
+def dataPi():
     tmp = 0
     hum = 0
     time_taken = 0
-    if ser:
-        while True:
-            read_serial = ser.readline()
-            print(read_serial)
-            read_serial = read_serial.strip()
-            split = read_serial.split(',');
+    with app.app_context():
+        if ser:
+            while True:
+                h_records = get_db().execute('SELECT * FROM humidity ORDER BY time DESC')
+                hum_single = h_records.fetchone()
+                h_records.close()
+                hum = hum_single[1]
+                t_records = get_db().execute('SELECT * FROM temperature ORDER BY time DESC')
+                tmp_single = t_records.fetchone()
+                t_records.close()
+                tmp = tmp_single[1]
 
-            if len(split) != 2:
-                continue
-
-            if 'Humidity' in split[0]:
-                hum_data = split[0].split(' ');
-                if (len(hum_data) != 2):
-                    continue
-                hum = hum_data[1]
-            else:
-                continue
-
-            if 'Temperature' in split[1]:
-                tmp_data = split[1].split(' ');
-                if (len(tmp_data) != 2):
-                    continue
-                tmp = tmp_data[1]
-            else:
-                continue
-
-            time_taken = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            break
-    sensorData=makeData(hum, tmp, time_taken)
+                # Use code below
+                # read_serial = ser.readline()
+                # print(read_serial)
+                # split = read_serial.split(',');
+                # if type(float(split[0])) is float:
+                #     hum = split[0]
+                # else:
+                #     continue
+                # if type(float(split[1])) is float:
+                #     tmp = split[1]
+                # else:
+                #     continue
+                time_taken = tmp_single[0]
+                break
     return render_template('data.html', humidity=hum, temperature=tmp, time_taken=time_taken)
 
 @app.route("/sensor_data")
@@ -178,13 +170,6 @@ def appendDataCSV(filename, data):
     with open(filename, 'a+') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(data)
-
-def makeData(humidity, temperature, time):
-    return {
-        'temperature': temperature,
-        'humidity': humidity,
-        'time': time
-    }
 
 def hrlyAvg(data):
     index = 0
@@ -218,9 +203,10 @@ if __name__ == "__main__":
     try:
         with open(data_file) as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader:
-                data.append(row)
-                print(row)
+            # for row in reader:
+            #     data.append(row)
+            #     print('This is that row')
+            #     print(row)
     except:
         createDataCSV(data_file)
 
@@ -228,7 +214,10 @@ if __name__ == "__main__":
 
     if (row_count == 0):
         createDataCSV(data_file)
-    hrlyAvg(data)
-    init_db()
+    # hrlyAvg(data)
+    # if not DATABASE:
+    #     print('Initializing db')
+    #     init_db()
+    print('Starting app')
     app.run(debug=True)
     #socketio.run(app, debug=True)
